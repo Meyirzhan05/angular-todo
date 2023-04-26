30714d90315a0319db1430127996f5f56567ea0f
Muratbekuli Meyirzhan <smilexah@inbox.ru>
2023-04-26 20:18:07 +0600

    Added some components and service which named categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CategoriesComponent} from '../views/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
